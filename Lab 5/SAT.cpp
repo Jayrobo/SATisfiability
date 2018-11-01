09d03381696b3ae552d96e920c95a0d8c698c987
Jayrobo <ciwang@sfu.ca>
2018-11-01 11:36:33 -0700

    setting up file reading and integer conversion

--- Lab 5/SAT.cpp
+++ Lab 5/SAT.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <ctype.h>
@@ -6,21 +7,37 @@ using namespace std;
 
 int main()
 {
-	char* filename = "Test_File.cnf";
+	char* filename = "small_test.cnf";
 	ifstream input_file;
 	input_file.open(filename);
+	string digits = "0123456789-"; //use for finding valid input
+	int maxi = 0;
+	int temp_int;
 
-	while (!input_file.eof)
+	while (!input_file.eof())
 	{
-		unsigned temp_storage;
+		string temp_storage;
 		input_file >> temp_storage;
 
-		if (isdigit(temp_storage))
+		if (temp_storage.find_first_of(digits) != string::npos) //the read in input should only contain number
 		{
-			if (temp_storage == 0)
-				cout << endl;
+			if (stoi(temp_storage) == 0)
+				cout << endl;	//end of a line in the .cnf file
+								//use to seperate clasues
 			else
-				cout << temp_storage << " ";
+			{
+				temp_int = stoi(temp_storage);
+				if (maxi < abs(temp_int))
+					maxi = abs(temp_int);
+
+				cout << temp_int << " ";
+			}
 		}
+		else
+			getline(input_file, temp_storage); //if the line contains character, we want to skip a line when reading
 	}
+
+	cout << "This is the number of variable to solve: " << maxi << endl;
+	system("pause");
+	return 0;
 }
